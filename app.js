47ed781514f26735bb08b8f8e1f3710b50118fc3
Abhishek Kulkarni <abhishekkul2000@gmail.com>
2022-09-29 00:10:52 +0530

    Fixed rating functionality and working on user selected rate

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ function userRating() {
   for (let rate = 0; rate < ratings.length; rate++) {
     // Listening for a click event on each number div
     ratings[rate].addEventListener("click", () => {
+      ratings[rate].classList.add("active");
+      ratings[rate].classList.contains.apply("active");
+      ratings[rate].classList.remove.apply("active");
       const value = ratings[rate].innerText;
       rates.innerHTML = `You have selected ${value} out of 5.`;
+      return value;
     });
   }
 }
-
 userRating();
+
 function ThankYou() {
   let thankYou = document.getElementById("star-icon");
   thankYou.innerHTML = `<img src="illustration-thank-you.svg" alt="ThankYou image">`;
@@ -24,6 +28,17 @@ function ThankYou() {
   const heading = document.getElementById("h2");
   heading.innerText = "Thank you!";
   heading.style.textAlign = "center";
+
+  rates.style.visibility = "visible";
+  rates.style.background = "var(--trans)";
+  rates.style.color = "var(--Orange)";
+  rates.style.height = "1.5em";
+  rates.style.textAlign = "center";
+  rates.style.position = "relative";
+  rates.style.left = "4em";
+  rates.style.width = "15em";
+  rates.style.borderRadius = "30px";
+  rates.style.display = "block";
 
   const para = document.getElementById("para");
   para.innerText = `We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!`;
