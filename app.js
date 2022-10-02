f76ef09db573e6419bdf0af45f7c42b71af41dd6
Abhishek Kulkarni <abhishekkul2000@gmail.com>
2022-10-02 15:09:53 +0530

    Fixed functionality

--- app.js
+++ app.js
@@ -1,52 +1,63 @@
+const para = document.getElementById("para");
+const heading = document.getElementById("h2");
 let ratings = document.querySelectorAll(".rating-list");
 let rates = document.querySelector("#rates");
+const ratingNumber = document.getElementById("rating-numbers");
+let submit = document.getElementById("submit");
+let thankYou = document.getElementById("star-icon");
+const sub = document.getElementById("submit");
 
-function userRating() {
-  for (let rate = 0; rate < ratings.length; rate++) {
-    // Listening for a click event on each number div
-    ratings[rate].addEventListener("click", () => {
-      ratings[rate].classList.add("active");
-      ratings[rate].classList.contains.apply("active");
-      ratings[rate].classList.remove.apply("active");
-      const value = ratings[rate].innerText;
-      rates.innerHTML = `You have selected ${value} out of 5.`;
-      return value;
-    });
-  }
+let value = undefined;
+
+for (let rate = 0; rate < ratings.length; rate++) {
+  // Listening for a click event on each number div
+  ratings[rate].addEventListener("click", () => {
+    // looping through the numbers to check for a previously selected rating and removing if such.
+    // Necessary in order to have only one rating selected at a time.
+    for (let i = 0; i < ratings.length; i++) {
+      ratings[i].classList.contains("active");
+      ratings[i].classList.remove("active");
+    }
+    // add the selected class which highlights the selected rating
+    ratings[rate].classList.add("active");
+    value = ratings[rate].innerText;
+
+    return value;
+  });
 }
-userRating();
 
 function ThankYou() {
-  let thankYou = document.getElementById("star-icon");
-  thankYou.innerHTML = `<img src="illustration-thank-you.svg" alt="ThankYou image">`;
-  thankYou.style.display = "flex";
-  thankYou.style.alignSelf = "center";
-  thankYou.style.width = "162px";
-  thankYou.style.height = "108px";
-  thankYou.style.background = "none";
+  if (value === undefined) {
+    alert("Please provide the ratings!");
+  } else {
+    rates.innerHTML = `You have selected ${value} out of 5.`;
 
-  const heading = document.getElementById("h2");
-  heading.innerText = "Thank you!";
-  heading.style.textAlign = "center";
+    thankYou.innerHTML = `<img src="illustration-thank-you.svg" alt="ThankYou image">`;
+    thankYou.style.display = "flex";
+    thankYou.style.alignSelf = "center";
+    thankYou.style.width = "162px";
+    thankYou.style.height = "108px";
+    thankYou.style.background = "none";
 
-  rates.style.visibility = "visible";
-  rates.style.background = "var(--trans)";
-  rates.style.color = "var(--Orange)";
-  rates.style.height = "1.5em";
-  rates.style.textAlign = "center";
-  rates.style.position = "relative";
-  rates.style.left = "4em";
-  rates.style.width = "15em";
-  rates.style.borderRadius = "30px";
-  rates.style.display = "block";
+    heading.innerText = "Thank you!";
+    heading.style.textAlign = "center";
 
-  const para = document.getElementById("para");
-  para.innerText = `We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!`;
-  para.style.textAlign = "center";
+    rates.style.visibility = "visible";
+    rates.style.background = "var(--trans)";
+    rates.style.color = "var(--Orange)";
+    rates.style.height = "1.5em";
+    rates.style.textAlign = "center";
+    rates.style.position = "relative";
+    rates.style.left = "4em";
+    rates.style.width = "15em";
+    rates.style.borderRadius = "30px";
+    rates.style.display = "block";
 
-  const ratings = document.getElementById("rating-numbers");
-  ratings.style.display = "none";
+    para.innerText = `We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!`;
+    para.style.textAlign = "center";
 
-  const sub = document.getElementById("submit");
-  sub.style.display = "none";
+    ratingNumber.style.display = "none";
+
+    sub.style.display = "none";
+  }
 }
